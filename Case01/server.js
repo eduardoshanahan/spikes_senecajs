979a53d92d1dcc22a56d45ec04400e58236facfa
Eduardo Shanahan <contact@eduardoshanahan.com>
2014-12-02 00:02:07 +0000

    Bit more example

--- Case01/server.js
+++ Case01/server.js
@@ -1,11 +1,33 @@
 var seneca = require('seneca')()
 
-seneca.add( {role:'math', cmd:'sum'}, function(args,callback) {
-  var sum = args.left + args.right
-  callback(null,{answer:sum})
-})
+seneca.add( 
+  {role:'math', cmd:'sum'}, 
+  function(args,callback) {
+    var sum = args.left + args.right
+    callback(null,{answer:sum})
+  }
+)
 
-seneca.act( {role:'math', cmd:'sum', left:1, right:2}, function(err,result) {
-  if( err ) return console.error( err )
-  console.log(result)
-})
+seneca.act( 
+  {role:'math', cmd:'sum', left:1, right:2}, 
+  function(err,result) {
+    if( err ) return console.error( err )
+    console.log(result)
+  }
+)
+
+seneca.add( 
+  {role:'math', cmd:'product'}, 
+  function(args,callback) {
+    var product = args.left * args.right
+    callback(null,{answer:product})
+  }
+)
+
+seneca.act( 
+  {role:'math', cmd:'product', left:3, right:4}, 
+  function(err,result) {
+    if( err ) return console.error( err )
+    console.log(result)
+  }
+)
