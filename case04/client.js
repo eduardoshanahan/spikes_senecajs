53834aa18c4eea15ddb8522f1151aa91e0bc6374
Eduardo Shanahan <contact@eduardoshanahan.com>
2014-12-16 16:20:24 +0000

    Bigger example

--- case04/client.js
+++ case04/client.js
@@ -10,13 +10,38 @@ seneca.add( {cmd:'salestax'}, function(args,callback){
   })
 })
 
-seneca.add( {cmd:'bananatax'}, function(args,callback){
-  seneca.act( {cmd:'bananas', prop:'rate'}, function(err,result){
-    var rate  = parseFloat(result.value)
-    var total = args.net * (1+rate)
-    callback(null,{total:total})
-  })
+// local rates
+seneca.add( {cmd:'salestax',country:'US'}, function(args,callback){
+  var state = {
+    'NY': 0.04,
+    'CA': 0.0625
+    // ..
+  }
+  var rate = state[args.state]
+  var total = args.net * (1+rate)
+  callback(null,{total:total})
 })
+
+
+// categories
+seneca.add( {cmd:'salestax',country:'IE'}, function(args,callback){
+  var category = {
+    'top': 0.23,
+    'reduced': 0.135
+    // ...
+  }
+  var rate = category[args.category]
+  var total = args.net * (1+rate)
+  callback(null,{total:total})
+})
+
+// seneca.add( {cmd:'bananatax'}, function(args,callback){
+//   seneca.act( {cmd:'bananas', prop:'rate'}, function(err,result){
+//     var rate  = parseFloat(result.value)
+//     var total = args.net * (1+rate)
+//     callback(null,{total:total})
+//   })
+// })
 
 seneca.client()
 
@@ -26,6 +51,18 @@ shop.salestax({net:100}, function(err,result){
   console.log( result.total )
 })
 
-shop.bananatax({net:1000}, function(err,result){
-  console.log( result.total )
+// shop.bananatax({net:1000}, function(err,result){
+//   console.log( result.total )
+// })
+
+shop.salestax({net:100,country:'DE'}, function(err,result){
+  console.log( 'DE: '+result.total )
+})
+
+shop.salestax({net:100,country:'US',state:'NY'}, function(err,result){
+  console.log( 'US,NY: '+result.total )
+})
+
+shop.salestax({net:100,country:'IE',category:'reduced'}, function(err,result){
+  console.log( 'IE: '+result.total )
 })
